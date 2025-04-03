9b3bc66c647c36e82f63832e61082930ae949462
deepsearock <zhaorui.p.wang@gmail.com>
2025-04-02 20:17:09 -0400

    change

--- include/cnn_launcher.cuh
+++ include/cnn_launcher.cuh
@@ -7,25 +7,20 @@
 #include "cnn.cuh"
 #include "utils.cuh"
 
-// Declare the texture reference for optimized kernel (declaration only)
+// Declare the texture reference (declaration only)
 extern texture<float, 2, cudaReadModeElementType> texRef;
 
 // Function prototypes
-
-// CPU performance measurement function
-PerformanceMetrics cnn_cpu(float *h_input, float *h_output, float *h_mask, 
+PerformanceMetrics cnn_cpu(float *h_input, float *h_output, float *h_mask,
                            int dimX, int dimY, int dimK);
 
-// Function to launch the naive convolution kernel
-PerformanceMetrics cnn_naive(float *h_input, float *h_output, float *h_mask, 
+PerformanceMetrics cnn_naive(float *h_input, float *h_output, float *h_mask,
                              int dimX, int dimY, int dimK);
 
-// Function to launch the optimized convolution kernel using texture memory
-PerformanceMetrics cnn_optimized(float *h_input, float *h_output, float *h_mask, 
+PerformanceMetrics cnn_optimized(float *h_input, float *h_output, float *h_mask,
                                  int dimX, int dimY, int dimK);
 
-// Function to launch the vectorized convolution kernel
-PerformanceMetrics cnn_vectorized(float *h_input, float *h_output, float *h_mask, 
+PerformanceMetrics cnn_vectorized(float *h_input, float *h_output, float *h_mask,
                                   int dimX, int dimY, int dimK);
 
-#endif // CNN_LAUNCHE
+#endif // CNN_LAUNCHER_CUH
